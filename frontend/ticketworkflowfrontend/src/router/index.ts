dd651c56beeec86656d7136cf0bfef2ca1a79973
Serenighter <nmrous16q@o2.pl>
2025-04-10 00:07:56 +0200

    Set up files for frontend

---
+++ frontend/ticketworkflowfrontend/src/router/index.ts
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import TicketsView from '../views/TicketsView.vue'
+
+const routes = [
+    {
+      path: '/',
+      redirect: '/tickets'
+    },
+    {
+      path: '/tickets',
+      component: TicketsView,
+      children: [
+        {
+          path: '',
+          name: 'tickets',
+          component: () => import('../components/TicketList.vue')
+        },
+        {
+          path: 'add-ticket',
+          name: 'add-ticket',
+          component: () => import('../components/TicketForm.vue')
+        },
+        {
+          path: 'profile',
+          name: 'profile',
+          component: () => import('../views/ProfileView.vue')
+        }
+      ]
+    }
+  ]
+  
+  const router = createRouter({
+    history: createWebHistory(),
+    routes
+  })
+  
+  export default router
